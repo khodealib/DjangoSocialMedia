9806d47e053f671f94bf691e5918fa6d8e88fce3
Ali Bagheri <khodealib@gmail.com>
2023-10-25 01:24:24 +0330

    Add DEV settings in settings file
    Changes to be committed:
    	black refactor:   DjangoSocialMedia/asgi.py
    	add debug mode settings:   DjangoSocialMedia/settings.py
    	add debug toolbar urls for debug mode:   DjangoSocialMedia/urls.py
    	black refactor:   DjangoSocialMedia/wsgi.py
    	black refactor:   manage.py

--- DjangoSocialMedia/urls.py
+++ DjangoSocialMedia/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from DjangoSocialMedia import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
