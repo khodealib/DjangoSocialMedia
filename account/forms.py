1246b4d0778e5ee6b1f383e6fae7ab3f8ca049c7
Ali Bagheri <khodealib@gmail.com>
2023-10-26 00:05:25 +0330

    username and email validation.

--- account/forms.py
+++ account/forms.py
@@ -1,28 +1,32 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "John",
-            }
-        )
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "John"}),
     )
     email = forms.EmailField(
         widget=forms.EmailInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "example@email.com",
-            }
-        )
+            attrs={"class": "form-control", "placeholder": "example@email.com"}
+        ),
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "your passwords",
-            }
-        )
+            attrs={"class": "form-control", "placeholder": "your passwords"}
+        ),
     )
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError("This email already exist.")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError("This username already exist.")
