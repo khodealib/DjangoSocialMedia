05250b002307c031066c8f5a944557fe9e267b4c
Ali Bagheri <khodealib@gmail.com>
2023-10-26 05:06:22 +0330

    add post detail view.

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from home import views
 app_name = "home"
 urlpatterns = [
     path("", views.HomeView.as_view(), name="index"),
+    path("post/<int:post_id>/<slug:post_slug>/", views.PostDetailView.as_view(), name="post_detail")
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+
+from home.models import Post
 
 
 class HomeView(View):
     template_name = "home/index.html"
 
-    def get(self, request):
-        return render(request, self.template_name)
+    def get(self, request: HttpRequest):
+        posts = Post.objects.all()
+        return render(request, self.template_name, {"posts": posts})
+
+
+class PostDetailView(View):
+    template_name = "home/detail.html"
+
+    def get(self, request: HttpRequest, post_id: int, post_slug: str):
+        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
+        return render(request, self.template_name, {"post": post})
