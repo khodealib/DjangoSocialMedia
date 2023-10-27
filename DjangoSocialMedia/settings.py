7ce1d5a7a3bea90f137cabbba213e9d41ba00e7e
Ali Bagheri <khodealib@gmail.com>
2023-10-27 15:27:57 +0330

    add email settings.

--- DjangoSocialMedia/settings.py
+++ DjangoSocialMedia/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -97,7 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "DjangoSocialMedia.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -107,7 +105,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -127,7 +124,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -138,7 +134,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -156,3 +151,12 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "account.authenticate.EmailBackend",
 ]
+
+# Google Email Settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "khodealib@gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = "crtlzrrseupumqoq"
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Ali's Website"
