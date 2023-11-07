23f66bcd57990f0a40b3b2ab280b8075f71dfc98
Ali Bagheri <khodelaib@gmail.com>
2023-11-07 10:47:16 +0330

    profile create signal.

---
+++ account/signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from account.models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    if kwargs["created"]:
+        Profile.objects.create(user=kwargs["instance"])
