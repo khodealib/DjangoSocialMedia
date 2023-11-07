a52ca7a3412aa704e247acfe6161d6455b7ea938
Ali Bagheri <khodelaib@gmail.com>
2023-11-07 08:27:02 +0330

    add profile model.

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-from account.models import Relation
+from account.models import Relation, Profile
 
 
 @admin.register(Relation)
 class RelationAdmin(admin.ModelAdmin):
     pass
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class ExtendedUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)
